03b4e7619d094551b923f4f98323eea9cc013cee
Diesirala <diegoesira@gmail.com>
2020-05-31 17:58:05 +0200

    Arreglo Bugs colocaccion fichas y soplido

--- src/board.cpp
+++ src/board.cpp
@@ -10,8 +10,14 @@ int Board::cambiarPosicion(int x, int y, int posicionx, int posiciony)
 	case 1://Turno de las blancas, tienes un movimiento y has selecionado una blanca
 		if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && posicionx == (x - 1) && (posiciony == (y - 1) || posiciony == (y + 1)))
 		{
+			estadSoplido();
+			for (int i = 0; i < 20; i++) {
+				if (PiezasSoplido[0][i] == tab[x][y])
+					PiezasSoplido[0][i].setCell(posicionx, posiciony, Object::QUEEN_GREEN);
+
+			}
 			tab[posicionx][posiciony].setCell(posicionx, posiciony, Object::QUEEN_GREEN);
-			tab[x][y].setCell(x, y, Object::EMPTY_CELL);
+			tab[x][y].setCell(-1, -1, Object::EMPTY_CELL);
 			movimientos = 0;
 			pasoTurno();
 			if (posicionx == 0)
@@ -23,8 +29,14 @@ int Board::cambiarPosicion(int x, int y, int posicionx, int posiciony)
 	case -2://turno de las negras tienes un movimiento y la pieza seleccionada es una negra
 		if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && posicionx == (x + 1) && (posiciony == (y - 1) || posiciony == (y + 1)))
 		{
+			estadSoplido();
+			for (int i = 0; i < 20; i++) {
+				if (PiezasSoplido[1][i] == tab[x][y])
+					PiezasSoplido[1][i].setCell(posicionx, posiciony, Object::QUEEN_BLACK);
+
+			}
 			tab[posicionx][posiciony].setCell(posicionx, posiciony, Object::QUEEN_BLACK);
-			tab[x][y].setCell(x, y, Object::EMPTY_CELL);
+			tab[x][y].setCell(-1, -1, Object::EMPTY_CELL);
 			movimientos = 0;
 			pasoTurno();
 			if (posicionx == (N-1))
@@ -55,18 +67,30 @@ int Board::cambiarPosicion(int x, int y, int posicionx, int posiciony)
 			if ((cont + 1) == abs(posicionx - x))
 			{
 				if (tab[x][y].getType() == Object::QUEEN_GREENR && turno == 1  && movimientos==1 ) {
+					estadSoplido();
+					for (int i = 0; i < 20; i++) {
+						if (PiezasSoplido[0][i] == tab[x][y])
+							PiezasSoplido[0][i].setCell(posicionx, posiciony, Object::QUEEN_GREENR);
+
+					}
 					tab[posicionx][posiciony].setCell(posicionx, posiciony, Object::QUEEN_GREENR);
 					movimientos = 0;
 					pasoTurno();
-					tab[x][y].setCell(x, y, Object::EMPTY_CELL);
+					tab[x][y].setCell(-1, -1, Object::EMPTY_CELL);
 					return 1;
 				}
 				if (tab[x][y].getType() == Object::QUEEN_BLACKR && turno == -1 && movimientos == 1 )
 				{
+					estadSoplido();
+					for (int i = 0; i < 20; i++) {
+						if (PiezasSoplido[1][i] == tab[x][y])
+							PiezasSoplido[1][i].setCell(posicionx, posiciony, Object::QUEEN_BLACK);
+
+					}
 					tab[posicionx][posiciony].setCell(posicionx, posiciony, Object::QUEEN_BLACKR);
 					movimientos = 0;
 					pasoTurno();
-					tab[x][y].setCell(x, y, Object::EMPTY_CELL);
+					tab[x][y].setCell(-1, -1, Object::EMPTY_CELL);
 					return 1;
 				}
 				else return 0;
@@ -96,11 +120,12 @@ int Board::comer(int x, int y, int posicionx, int posiciony)
 				{
 
 					tab[posicionx][posiciony].setCell(posicionx, posiciony, Object::QUEEN_GREEN);
-					tab[x][y].setCell(x, y, Object::EMPTY_CELL);
-					tab[pmediox][pmedioy].setCell(pmediox, pmedioy, Object::EMPTY_CELL);
+					tab[x][y].setCell(-1, -1, Object::EMPTY_CELL);
+					tab[pmediox][pmedioy].setCell(-1, -1, Object::EMPTY_CELL);
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					negras--;
+					estadSoplido();
 					if (posicionx == 0) {
 						reina(posicionx, posiciony);
 						pasoTurno();
@@ -122,11 +147,12 @@ int Board::comer(int x, int y, int posicionx, int posiciony)
 				{
 
 					tab[posicionx][posiciony].setCell(posicionx, posiciony, Object::QUEEN_BLACK);
-					tab[x][y].setCell(x, y, Object::EMPTY_CELL);
-					tab[pmediox][pmedioy].setCell(pmediox, pmedioy, Object::EMPTY_CELL);
+					tab[x][y].setCell(-1, -1, Object::EMPTY_CELL);
+					tab[pmediox][pmedioy].setCell(-1, -1, Object::EMPTY_CELL);
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					blancas--;
+					estadSoplido();
 					if (posicionx == (N - 1)) {
 						reina(posicionx, posiciony);
 						pasoTurno();
@@ -162,23 +188,24 @@ int Board::comer(int x, int y, int posicionx, int posiciony)
 					{
 						if (tab[x][y].getType() == Object::QUEEN_GREENR && turno == 1) {
 							tab[posicionx][posiciony].setCell(posicionx, posiciony, Object::QUEEN_GREENR);
-							tab[posicionx - u][posiciony - k].setCell(posicionx - u, posiciony - k, Object::EMPTY_CELL);
-							tab[x][y].setCell(x, y, Object::EMPTY_CELL);
+							tab[posicionx - u][posiciony - k].setCell(-1, -1, Object::EMPTY_CELL);
+							tab[x][y].setCell(-1, -1, Object::EMPTY_CELL);
 							pmovida = tab[posicionx][posiciony];
 							movimientos = 0;
 							negras--;
+							estadSoplido();
 							return 1;
 
 						}
 						if (tab[x][y].getType() == Object::QUEEN_BLACKR && turno == -1)
 						{
 							tab[posicionx][posiciony].setCell(posicionx, posiciony, Object::QUEEN_BLACKR);
-							tab[posicionx - u][posiciony - k].setCell(posicionx - u, posiciony - k, Object::EMPTY_CELL);
-							tab[x][y].setCell(x, y, Object::EMPTY_CELL);
+							tab[posicionx - u][posiciony - k].setCell(-1, -1, Object::EMPTY_CELL);
+							tab[x][y].setCell(-1, -1, Object::EMPTY_CELL);
 							pmovida = tab[posicionx][posiciony];
 							movimientos = 0;
 							blancas--;
-
+							estadSoplido();
 							return 1;
 						}
 						else return 0;
@@ -192,7 +219,9 @@ int Board::comer(int x, int y, int posicionx, int posiciony)
 			;
 			break;
 		}
+		return 0;
 	}
+	return 0;
 }
 
 est Board::estadoPartida(void) {
@@ -224,100 +253,48 @@ void Board::reina(int posicionx, int posiciony)
 
 void Board::actualizarEstado(void)
 {
-	Piece vacio[20];
-	if (turno == 1) {
-		for (int i = 0; i < 20; i++)
-			PiezasSoplido[0][i] = vacio[i];
-	}
-	if (turno == -1) {
-		for (int i = 0; i < 20; i++)
-			PiezasSoplido[1][i] = vacio[i];
-	}
-	int posDeComer=0;
-	int posDeMover=0;
+
+	
 	
 	if (blancas == 0) //GANAN NEGRAS
 		estado = DERROTAB;
 	if (negras == 0)  //GANAN BLANCAS
 		estado = VICTORIAB;
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			for (int k = 0; k < N; k++) {
-				for (int l = 0; l < N; l++) {
-
-					Board copia(*this);
-					if (copia.comer(i, j, k, l) == 1) {
-						
-						
-						if(turno==1)
-						PiezasSoplido[0][posDeComer] = tab[i][j];
-						if(turno==-1)
-							PiezasSoplido[1][posDeComer] = tab[i][j];
-						posDeComer++;
-						
-					}
-					if (copia.cambiarPosicion(i, j, k, l) == 1)
-						posDeMover++;
-
-
-				}
-			}
-
-		}
-
-
-	}
 	
-	for (int i = 0; i < posDeComer; i++) {
-		int c=0, d=0;
-		if (turno == 1 && PiezasSoplido[1][i].getType() != Object::EMPTY_CELL)
-
-			PiezasSoplido[1][i].getCellNumber(c, d);
-		if (turno == -1 && PiezasSoplido[0][i].getType() != Object::EMPTY_CELL) 
-			PiezasSoplido[0][i].getCellNumber(c, d);
-			cout << "Piezas soplido " << c << d << endl;
-	
-
-	}
-
-	
-	if (posDeComer == 0 && posDeMover == 0 && movimientos==1)
-		estado = TABLAS;
-
-
-	cout << "Las posibilidades de comer son: " << posDeComer << endl;
-	cout << "Las posibilidades de moverse son: " << posDeMover << endl;
-	//delete[] PiezasSoplido;
 }
 
 void Board::pasoTurno(void) {
+
 	if (movimientos == 0) {
+	
 		movimientos = 1;
 		turno = -turno;
-		
+		//estadSoplido();
 	}
+	
+	
 }
 int Board::soplido(int x, int y)
 {
 	for (int i = 0; i < 20; i++) {
 		int c, d;
-		PiezasSoplido[1][i].getCellNumber(c, d);
-		if (turno == 1 && movimientos==1 && (tab[x][y].getType() == Object::QUEEN_BLACK || tab[x][y].getType() == Object::QUEEN_BLACKR)) {
-			
+		
+		if ((turno == 1 && movimientos==1 && tab[x][y].getType() == Object::QUEEN_BLACK) || (turno == 1 && movimientos == 1 && tab[x][y].getType() == Object::QUEEN_BLACKR)) {
+			PiezasSoplido[1][i].getCellNumber(c, d);
 			if (c == x && d == y) {
-				tab[x][y].setCell(x, y, Object::EMPTY_CELL);
-				PiezasSoplido[x][y].setCell(-1, -1, Object::EMPTY_CELL);
+				tab[x][y].setCell(-1, -1, Object::EMPTY_CELL);
+				estadSoplido();
 				negras--;
 				return 1;
 			}
 			
 		}
-		if (turno == -1 && movimientos==1 && (tab[x][y].getType()== Object::QUEEN_GREEN|| tab[x][y].getType() == Object::QUEEN_GREENR)) {
+		if ((turno == -1 && movimientos == 1 && tab[x][y].getType() == Object::QUEEN_GREEN) || (turno == -1 && movimientos == 1 && tab[x][y].getType() == Object::QUEEN_GREENR)) {
 			PiezasSoplido[0][i].getCellNumber(c, d);
 			if (c == x && d == y) {
-				tab[x][y].setCell(x, y, Object::EMPTY_CELL);
-				PiezasSoplido[x][y].setCell(-1, -1, Object::EMPTY_CELL);
+				tab[x][y].setCell(x-1, -1, Object::EMPTY_CELL);
+				estadSoplido();
+				//PiezasSoplido[0][i].setCell(-1, -1, Object::EMPTY_CELL);
 				blancas--;
 				return 1;
 			}
@@ -325,5 +302,90 @@ int Board::soplido(int x, int y)
 	}
 	return 0;
 }
+
+
+void Board::estadSoplido(void)
+{
+
+	if (cop == 0) {
+		Piece vacio[20];
+		if (turno == 1) {
+			for (int i = 0; i < 20; i++)
+				PiezasSoplido[0][i] = vacio[i];
+		}
+		if (turno == -1) {
+			for (int i = 0; i < 20; i++)
+				PiezasSoplido[1][i] = vacio[i];
+		}
+		int aux0=0,aux1=0;
+		int posDeComer = 0;
+		int posDeMover = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < N; k++) {
+					for (int l = 0; l < N; l++) {
+
+						Board copia(*this);
+						if (copia.comer(i, j, k, l) == 1) {
+
+
+							if ((turno == 1 && posDeComer > 0) && PiezasSoplido[0][posDeComer - 1] == tab[i][j]) {
+								posDeComer++;
+								aux0 ++;
+								continue;
+							}
+							
+							if ((turno == -1 && posDeComer > 0) && PiezasSoplido[1][posDeComer - 1] == tab[i][j]) {
+								posDeComer++;
+								aux1++;
+								continue;
+							}
+							if (turno == 1)
+								PiezasSoplido[0][posDeComer - aux0] = tab[i][j];
+							if (turno == -1)
+								PiezasSoplido[1][posDeComer-aux1] = tab[i][j];
+							posDeComer++;
+							//j++;
+
+						}
+						if (copia.cambiarPosicion(i, j, k, l) == 1)
+							posDeMover++;
+
+
+					}
+				}
+
+			}
+
+
+		}
+
+		for (int i = 0; i < 20; i++) {
+			int c = -1, d = -1, e = -1, f = -1;
+		
+
+				PiezasSoplido[1][i].getCellNumber(c, d);
+			
+				PiezasSoplido[0][i].getCellNumber(e, f);
+			cout << "Piezas que pueden soplar las blancas: " << c << d << endl;
+
+			cout << "Piezas que pueden soplar las negras: " << e << f << endl;
+
+		}
+
+
+		if (posDeComer == 0 && posDeMover == 0 && movimientos == 1)
+			estado = TABLAS;
+		cout << "Las posibilidades de comer son: " << posDeComer << endl;
+		cout << "Las posibilidades de moverse son: " << posDeMover << endl;
+	}
+
+
+
+
+}
+
+
+
 
 //Prueba 3 de GITHUB
