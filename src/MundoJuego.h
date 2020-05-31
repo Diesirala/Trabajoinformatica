5fd3726b47d42b276cca03946db13085e30a72d5
Juancito <juanmadiz123@gmail.com>
2020-05-31 12:24:33 +0200

    Inclusion reina

---
+++ src/MundoJuego.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#pragma once
+#include "ListaPiezas.h"
+//#include "Tablero.h"
+class Mundo
+    {
+    private:
+        enum tipodejuego { CHINA, ESPAN, PERUANA };
+        tipodejuego tipo;
+        ListaPiezas Piezas;
+        Tablero tab;
+        float x_ojo;
+        float y_ojo;
+        float z_ojo;
+        bool impacto;
+        int nivel;
+        int NumFichas;
+
+    public:
+        Mundo();
+        ~Mundo();
+        void tecla(unsigned char key);
+        void teclaEspecial(unsigned char key);
+        void inicializa(int);
+        void rotarOjo();
+        void mueve();
+        void dibuja();
+        bool getImpacto();
+        void Limpiar();
+ };
+
+
+
+
+
+
