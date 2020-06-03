e693eadd82e7a1963426d60273f1793cb8683c9c
Diesirala <diegoesira@gmail.com>
2020-06-03 14:40:54 +0200

    Añadida IA en variante Turca

--- src/board.cpp
+++ src/board.cpp
@@ -126,12 +126,12 @@ int Board::comer(int x, int y, int posicionx, int posiciony)
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					negras--;
-					estadSoplido();
+					
 					if (posicionx == 0) {
 						reina(posicionx, posiciony);
 						
 					}
-
+					estadSoplido();
 					return 1;
 				}
 				else return 0;
@@ -153,12 +153,12 @@ int Board::comer(int x, int y, int posicionx, int posiciony)
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					blancas--;
-					estadSoplido();
+					
 					if (posicionx == (N - 1)) {
 						reina(posicionx, posiciony);
 						
 					}
-
+					estadSoplido();
 					return 1;
 				}
 				else return 0;
@@ -543,12 +543,12 @@ int BoardCheckers::comer(int x, int y, int posicionx, int posiciony)
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					negras--;
-					estadSoplido();
+					
 					if (posicionx == 0) {
 						reina(posicionx, posiciony);
 						
 					}
-
+					estadSoplido();
 					return 1;
 				}
 				else return 0;
@@ -570,12 +570,12 @@ int BoardCheckers::comer(int x, int y, int posicionx, int posiciony)
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					blancas--;
-					estadSoplido();
+					
 					if (posicionx == (N - 1)) {
 						reina(posicionx, posiciony);
 						
 					}
-
+					estadSoplido();
 					return 1;
 				}
 				else return 0;
@@ -770,12 +770,11 @@ int BoardItalianas::comer(int x, int y, int posicionx, int posiciony)
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					negras--;
-					estadSoplido();
 					if (posicionx == 0) {
 						reina(posicionx, posiciony);
 
 					}
-
+					estadSoplido();
 					return 1;
 				}
 				else return 0;
@@ -797,12 +796,11 @@ int BoardItalianas::comer(int x, int y, int posicionx, int posiciony)
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					blancas--;
-					estadSoplido();
 					if (posicionx == (N - 1)) {
 						reina(posicionx, posiciony);
 
 					}
-
+					estadSoplido();
 					return 1;
 				}
 				else return 0;
@@ -999,10 +997,6 @@ int BoardPeruanas::comer(int x, int y, int posicionx, int posiciony)
 					movimientos = 0;
 					negras--;
 					estadSoplido();
-					if (posicionx == 0)
-						reina(posicionx, posiciony);
-					
-
 					return 1;
 				}
 				else return 0;
@@ -1025,9 +1019,6 @@ int BoardPeruanas::comer(int x, int y, int posicionx, int posiciony)
 					movimientos = 0;
 					blancas--;
 					estadSoplido();
-					if (posicionx == (N - 1))
-						reina(posicionx, posiciony);
-					
 					return 1;
 				}
 				else return 0;
@@ -1230,9 +1221,6 @@ int BoardPeruanasIA::comer(int x, int y, int posicionx, int posiciony)
 					movimientos = 0;
 					negras--;
 					estadSoplido();
-					if (posicionx == 0)
-						reina(posicionx, posiciony);
-					
 					return 1;
 				}
 				else return 0;
@@ -1255,8 +1243,7 @@ int BoardPeruanasIA::comer(int x, int y, int posicionx, int posiciony)
 					movimientos = 0;
 					blancas--;
 					estadSoplido();
-					if (posicionx == (N - 1))
-						reina(posicionx, posiciony);
+				
 				
 					return 1;
 				}
@@ -1505,12 +1492,12 @@ int BoardEspañolas::comer(int x, int y, int posicionx, int posiciony)
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					negras--;
-					estadSoplido();
+					
 					if (posicionx == 0) {
 						reina(posicionx, posiciony);
 
 					}
-
+					estadSoplido();
 					return 1;
 				}
 				else return 0;
@@ -1532,12 +1519,11 @@ int BoardEspañolas::comer(int x, int y, int posicionx, int posiciony)
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					blancas--;
-					estadSoplido();
 					if (posicionx == (N - 1)) {
 						reina(posicionx, posiciony);
 
 					}
-
+					estadSoplido();
 					return 1;
 				}
 				else return 0;
@@ -1739,12 +1725,12 @@ int BoardEspañolasIA::comer(int x, int y, int posicionx, int posiciony)
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					negras--;
-					estadSoplido();
+					
 					if (posicionx == 0) {
 						reina(posicionx, posiciony);
 
 					}
-
+					estadSoplido();
 					return 1;
 				}
 				else return 0;
@@ -1766,12 +1752,12 @@ int BoardEspañolasIA::comer(int x, int y, int posicionx, int posiciony)
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					blancas--;
-					estadSoplido();
+					
 					if (posicionx == (N - 1)) {
 						reina(posicionx, posiciony);
 
 					}
-
+					estadSoplido();
 					return 1;
 				}
 				else return 0;
@@ -1960,7 +1946,7 @@ int BoardTurcas::cambiarPosicion(int x, int y, int posicionx, int posiciony)
 		switch (tab[x][y].getType() * movimientos * turno)// Enumeracion por el turno
 		{
 		case 1://Turno de las blancas, tienes un movimiento y has selecionado una blanca
-			if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && (posicionx == (x - 1) ^ posiciony == (y - 1) ^ posiciony == (y + 1)))
+			if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && ((posicionx == (x - 1) && posiciony==y) ^ (posiciony == (y - 1) && posicionx == x) ^ (posiciony == (y + 1) && posicionx == x)))
 			{
 				estadSoplido();
 				for (int i = 0; i < 20; i++) {
@@ -1975,13 +1961,12 @@ int BoardTurcas::cambiarPosicion(int x, int y, int posicionx, int posiciony)
 				pasoTurno();
 				if (posicionx == 0)
 					reina(posicionx, posiciony);
-				else pasoTurno();
 				return 1;
 			}
 			else return 0;
 			break;
 		case -2://turno de las negras tienes un movimiento y la pieza seleccionada es una negra
-			if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && (((posicionx == (x + 1)) ^ (posiciony == (y - 1))) ^ (posiciony == (y + 1))))
+			if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && ((posicionx == (x + 1) && posiciony == y) ^ (posiciony == (y - 1) && posicionx == x) ^ (posiciony == (y + 1) && posicionx == x)))
 			{
 				estadSoplido();
 				for (int i = 0; i < 20; i++) {
@@ -1993,9 +1978,9 @@ int BoardTurcas::cambiarPosicion(int x, int y, int posicionx, int posiciony)
 				tab[posicionx][posiciony].setCell(posicionx, posiciony, Object::QUEEN_BLACK);
 				tab[x][y].setCell(-1, -1, Object::EMPTY_CELL);
 				movimientos = 0;
+				pasoTurno();
 				if (posicionx == (N - 1))
 					reina(posicionx, posiciony);
-				else pasoTurno();
 				return 1;
 			}
 			else return 0;
@@ -2005,7 +1990,7 @@ int BoardTurcas::cambiarPosicion(int x, int y, int posicionx, int posiciony)
 			break;
 
 		default: // vamos a utilizarlo para las reinas que se mueven las dos igual
-			/*if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && ((posiciony == y) ^ (posicionx == x)))
+			if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && ((posiciony == y) ^ (posicionx == x)))
 			{
 				int u, k, cont = 0;
 				if (posicionx - x < 0)
@@ -2014,12 +1999,19 @@ int BoardTurcas::cambiarPosicion(int x, int y, int posicionx, int posiciony)
 				if (posiciony - y < 0)
 					k = -1;
 				else k = 1;
-				for (int i = x, j = y; j != posiciony; i += u, j += k)
+				if(posiciony == y)
+				for (int i = x;  i!= posicionx; i += u)
 				{
-					if (tab[i][j].getType() == Object::EMPTY_CELL)
+					if (tab[i][y].getType() == Object::EMPTY_CELL)
 						cont++;
 				}
-				if ((cont + 1) == abs(posicionx - x))
+				if (posicionx == x)
+				for (int  j = y; j != posiciony;  j += k)
+				{
+					if (tab[x][j].getType() == Object::EMPTY_CELL)
+						cont++;
+				}
+				if ((cont + 1) == ((abs(posicionx - x)^ (abs(posiciony - y)))))
 				{
 					if (tab[x][y].getType() == Object::QUEEN_GREENR && turno == 1 && movimientos == 1) {
 						estadSoplido();
@@ -2055,7 +2047,7 @@ int BoardTurcas::cambiarPosicion(int x, int y, int posicionx, int posiciony)
 				}
 				else return 0;
 			}
-			else return 0;*/
+			else return 0;
 			break;
 		}
 
@@ -2082,7 +2074,7 @@ int BoardTurcas::comer(int x, int y, int posicionx, int posiciony)
 		switch (tab[x][y].getType() * turno)
 		{
 		case 1:
-			if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && (posicionx == (x - 2) ^ posiciony == (y - 2) ^ posiciony == (y + 2)))
+			if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && ((posicionx == (x - 2)&& posiciony==y) ^ (posiciony == (y - 2) && posicionx == x) ^ (posiciony == (y + 2) && posicionx == x)))
 			{
 				int pmediox, pmedioy;
 				pmediox = (posicionx + x) / 2;
@@ -2097,12 +2089,12 @@ int BoardTurcas::comer(int x, int y, int posicionx, int posiciony)
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					negras--;
-					estadSoplido();
+					
 					if (posicionx == 0) {
 						reina(posicionx, posiciony);
 
 					}
-
+					estadSoplido();
 					return 1;
 				}
 				else return 0;
@@ -2110,7 +2102,7 @@ int BoardTurcas::comer(int x, int y, int posicionx, int posiciony)
 			else return 0;
 			break;
 		case -2:
-			if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && (posicionx == (x + 2) ^ posiciony == (y - 2) ^ posiciony == (y + 2)))
+			if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && ((posicionx == (x + 2) && posiciony == y) ^ (posiciony == (y - 2) && posicionx == x) ^ (posiciony == (y + 2) && posicionx == x)))
 			{
 				int pmediox, pmedioy;
 				pmediox = (posicionx + x) / 2;
@@ -2124,12 +2116,12 @@ int BoardTurcas::comer(int x, int y, int posicionx, int posiciony)
 					pmovida = tab[posicionx][posiciony];
 					movimientos = 0;
 					blancas--;
-					estadSoplido();
+					
 					if (posicionx == (N - 1)) {
 						reina(posicionx, posiciony);
 
 					}
-
+					estadSoplido();
 					return 1;
 				}
 				else return 0;
@@ -2140,28 +2132,45 @@ int BoardTurcas::comer(int x, int y, int posicionx, int posiciony)
 			return 0;
 			break;
 		default:// vamos a utilizarlo para las reinas que se mueven las dos igual
-			if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && (posiciony==y  ^  posicionx==x))
+			if (tab[posicionx][posiciony].getType() == Object::EMPTY_CELL && ((posiciony==y)  ^  (posicionx==x)))
 			{
-				int u, k, cont = 0, aux1 = 0, aux2 = 0;
-				if (posicionx - x < 0)
+				int u=0, k=0, cont = 0, aux1 = 0, aux2 = 0;
+				if (posicionx - x < 0 )
 					u = -1;
-				else u = 1;
+				else if (posicionx - x > 0) u = 1;
 				if (posiciony - y < 0)
 					k = -1;
-				else k = 1;
+				else if (posiciony - y > 0) k = 1;
 				if ((tab[x][y].getType() == Object::QUEEN_GREENR) && (tab[posicionx - u][posiciony - k].getType() % 2 == 0 && tab[posicionx - u][posiciony - k].getType() != 0) || (tab[x][y].getType() == Object::QUEEN_BLACKR) && (tab[posicionx - u][posiciony - k].getType() % 2 != 0 && tab[posicionx - u][posiciony - k].getType() != 0))
 				{
-					for (int i = x, j = y; i != (posicionx - u) ,j!= (posiciony - k); i += u, j += k)
-					{
-						if (tab[i][j].getType() == Object::EMPTY_CELL)
-							cont++;
-					}
-					if ((cont + 2) == (abs(posicionx - x)))
+					int u, k, cont = 0;
+					if (posicionx - x < 0)
+						u = -1;
+					else u = 1;
+					if (posiciony - y < 0)
+						k = -1;
+					else k = 1;
+					if (posiciony == y)
+						for (int i = x; i != (posicionx-u); i += u)
+						{
+							if (tab[i][y].getType() == Object::EMPTY_CELL)
+								cont++;
+						}
+					if (posicionx == x)
+						for (int j = y; j != (posiciony-k); j += k)
+						{
+							if (tab[x][j].getType() == Object::EMPTY_CELL)
+								cont++;
+						}
+					if ((cont + 2) == ((abs(posicionx - x) ^ (abs(posiciony - y)))))
 					{
 						if (tab[x][y].getType() == Object::QUEEN_GREENR && turno == 1) {
 							ETSIDI::play("sonidos/comer.wav");
 							tab[posicionx][posiciony].setCell(posicionx, posiciony, Object::QUEEN_GREENR);
-							tab[posicionx - u][posiciony - k].setCell(-1, -1, Object::EMPTY_CELL);
+							if(x==posicionx)
+							tab[posicionx][posiciony - k].setCell(-1, -1, Object::EMPTY_CELL);
+							if(posiciony==y)
+							tab[posicionx-u][posiciony].setCell(-1, -1, Object::EMPTY_CELL);
 							tab[x][y].setCell(-1, -1, Object::EMPTY_CELL);
 							pmovida = tab[posicionx][posiciony];
 							movimientos = 0;
@@ -2174,7 +2183,10 @@ int BoardTurcas::comer(int x, int y, int posicionx, int posiciony)
 						{
 							ETSIDI::play("sonidos/comer.wav");
 							tab[posicionx][posiciony].setCell(posicionx, posiciony, Object::QUEEN_BLACKR);
-							tab[posicionx - u][posiciony - k].setCell(-1, -1, Object::EMPTY_CELL);
+							if (x == posicionx)
+								tab[posicionx][posiciony - k].setCell(-1, -1, Object::EMPTY_CELL);
+							if (posiciony == y)
+								tab[posicionx - u][posiciony].setCell(-1, -1, Object::EMPTY_CELL);
 							tab[x][y].setCell(-1, -1, Object::EMPTY_CELL);
 							pmovida = tab[posicionx][posiciony];
 							movimientos = 0;
@@ -2199,7 +2211,124 @@ int BoardTurcas::comer(int x, int y, int posicionx, int posiciony)
 }
 void BoardTurcas::estadSoplido(void)
 {
+	if (cop == 0) {
+		Piece vacio[20];
+		if (turno == 1) {
+			for (int i = 0; i < 20; i++)
+				PiezasSoplido[0][i] = vacio[i];
+
+		}
+		if (turno == -1) {
+			for (int i = 0; i < 20; i++)
+				PiezasSoplido[1][i] = vacio[i];
+
+		}
+		int aux0 = 0, aux1 = 0;
+		int posDeComer = 0;
+		int posDeMover = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < N; k++) {
+					for (int l = 0; l < N; l++) {
+
+						BoardTurcas copia(*this);
+						if (copia.comer(i, j, k, l) == 1) {
+
+
+							if ((turno == 1 && posDeComer > 0) && PiezasSoplido[0][posDeComer - 1] == tab[i][j]) {
+								posDeComer++;
+								aux0++;
+								continue;
+							}
+
+							if ((turno == -1 && posDeComer > 0) && PiezasSoplido[1][posDeComer - 1] == tab[i][j]) {
+								posDeComer++;
+								aux1++;
+								continue;
+							}
+							if (turno == 1)
+								PiezasSoplido[0][posDeComer - aux0] = tab[i][j];
+							if (turno == -1)
+								PiezasSoplido[1][posDeComer - aux1] = tab[i][j];
+							posDeComer++;
+							//j++;
+
+						}
+						if (copia.cambiarPosicion(i, j, k, l) == 1)
+							posDeMover++;
+
+
+					}
+				}
+
+			}
+
+
+		}
+
+		for (int i = 0; i < 20; i++) {
+			int c = -1, d = -1, e = -1, f = -1;
+
+
+			PiezasSoplido[1][i].getCellNumber(c, d);
+
+			PiezasSoplido[0][i].getCellNumber(e, f);
+			cout << "Piezas que pueden soplar las blancas: " << c << d << endl;
+
+			cout << "Piezas que pueden soplar las negras: " << e << f << endl;
+
+		}
+	}
+}
+void BoardTurcas::actualizarEstado(void)
+{
+	if (cop == 0) {
+		int posDeComer = 0;
+		int posDeMover = 0;
+
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < N; k++) {
+					for (int l = 0; l < N; l++) {
+
+						BoardTurcas copia(*this);
+						if (copia.comer(i, j, k, l) == 1)
+							posDeComer++;
+						if (copia.cambiarPosicion(i, j, k, l) == 1)
+							posDeMover++;
+
+
+					}
+				}
+
+			}
+
+
+		}
+		if (posDeComer == 0 && posDeMover == 0 && movimientos == 1)
+			estado = TABLAS;
+
+		if (blancas == 0) //GANAN NEGRAS
+			estado = DERROTAB;
+		if (negras == 0)  //GANAN BLANCAS
+			estado = VICTORIAB;
+
+		cout << "Las posibilidades de comer son: " << posDeComer << endl;
+		cout << "Las posibilidades de moverse son: " << posDeMover << endl;
+	}
+}
+void BoardTurcas::pasoTurno(void)
+{
+	if (movimientos == 0) {
+
+		movimientos = 1;
+		turno = -turno;
+
+		actualizarEstado();
+		//estadSoplido();
+	}
+
 
 }
-
 //Prueba 3 de GITHUB
